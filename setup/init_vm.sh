7e393a2a133ffd53c42ba51e7f797b26b68e9ea1
Wayko <franck@chvl.re>
2024-11-22 00:03:38 +0100

    feat(init_vm.sh): Added local default

--- setup/init_vm.sh
+++ setup/init_vm.sh
@@ -9,18 +9,19 @@ ln -s agetty /etc/init.d/agetty.ttyS0
 echo ttyS0 > /etc/securetty
 rc-update add agetty.ttyS0 default
 rc-update add root default
+rc-update add local default
 
 echo "root:root" | chpasswd
-adduser user -D
+adduser -D 
 echo "user:user" | chpasswd
 
 rc-update add devfs boot
 rc-update add procfs boot
 rc-update add sysfs boot
+rc-update add localmount boot
 
 rc-update add syslog boot
 rc-update add networking boot
-rc-update add rootkit default
 
 for d in bin etc lib root sbin usr; do tar c "/$d" | tar x -C /my-rootfs; done
 for dir in dev proc run sys var; do mkdir /my-rootfs/${dir}; done
